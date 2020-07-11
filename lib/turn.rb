de0aef6d442c6eda65d741ebd3c00df153ca8a95
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-11 10:28:14 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,35 @@ def input_to_index(input)
   input.to_i - 1
   # users count from 1, Ruby from 0
 end
+
+#valid_move?
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    true
+  else
+   false
+  end
+end
+def position_taken?(board, index)
+
+  if board[index] == " " || board[index] == "" || board[index] == nil
+  	false
+  else
+   true
+
+  end
+#move allows "X" player in the bottom right and "O" in the top left
+
+#turn
+   #asks the user for input by printing: "Please enter 1-9:" (FAILED - 3)
+   #gets the user input (FAILED - 4)
+   #calls the input_to_index method (FAILED - 5)
+   #validates the input correctly (FAILED - 6)
+   #asks for input again after a failed validation (FAILED - 7)
+   #makes valid moves (FAILED - 8)
+   #displays a correct board after a valid turn (FAILED - 9)
+
+
+
+
+#
